a1a34094003e717592938cbd7947b761def3c460
AlbertoPMDM <albertopmdm@gmail.com>
2024-06-12 13:35:33 -0600

    se puso algo por si se pierde conexion con algun sensor

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import requests
 import numpy as np
 import json
+import sqlite3
 
 def get_data(sensor: str) -> np.ndarray:
     '''
@@ -15,14 +16,36 @@ def get_data(sensor: str) -> np.ndarray:
     
     {"data":"[0.8014442490425848, 0.12287946936057148, ...]"}
 
+    si no se obtiene, devuelve None
     '''
-    request = requests.get(sensor, stream=True)
+    try:
+        request = requests.get(sensor, stream=True, timeout=0.1)
+        return np.array(
+            json.loads(request.json()['data'])
+        )
+    except:
+        print(f'Hay un problema con {sensor}')
+        return None
+        
 
-    return np.array(
-        json.loads(request.json()['data'])
+def avg_data(*arrays: np.ndarray) -> np.ndarray:
+    '''
+    toma arreglos [a, b, c] y devuelve [a_avg, b_avg, c_avg]
+
+    Si un arreglo es none, lo ignora
+    '''
+    to_avg = []
+    for array in arrays:
+        if array is not None:
+            to_avg.append(array)
+    return np.average(
+        np.array(to_avg), axis=0
     )
 
-def to_db(db):
-    ...
+sensors = ['http://127.0.0.1:100', 'http://127.0.0.1:101', 'http://127.0.0.1:102']
+sensor_data = [get_data(sensor) for sensor in sensors]
 
-print(get_data("http://192.168.1.12:105/"))
+print(sensor_data)
+print(avg_data(*sensor_data))
+
+
