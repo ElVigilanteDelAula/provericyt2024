dc948682ec73e3573f4f5d6c0e133b6a1866f1b8
AlbertoPMDM <albertopmdm@gmail.com>
2024-07-18 21:18:32 -0600

    da enteros como el sensor

--- src/py/test_server.py
+++ src/py/test_server.py
@@ -3,13 +3,15 @@ import numpy as np
 
 app = Flask(__name__)
 
+rng = np.random.default_rng()
+
 @app.route("/", methods=['GET'])
 def data():
     '''
     simula el funcionamiento del esp8266
     '''
     return jsonify(
-        {"data":np.random.random_sample((11)).tolist()}
+        {"data":rng.integers(0,100,(11)).tolist()}
     )
 
 if __name__ =="__main__":
